8ef521b9d56b589d5584837aa167760e272ad4c6
nickm <nickmelnikov219@gmail.com>
2026-02-01 20:23:31 -0500

    fixed score

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Test");
-
 getLocation();
 
 async function initMap(lat, lon) {
@@ -20,18 +18,18 @@ async function initMap(lat, lon) {
         fullscreenControl: true,
     });
     // Add a marker positioned at the current location.
-    const marker = new AdvancedMarkerElement({
+    const draggableMarker = new AdvancedMarkerElement({
         map: innerMap,
         position: {lat: lat, lng: lon},
         title: 'Current Position',
         gmpDraggable: true,
     });
-    mapElement.append(marker);
+    mapElement.append(draggableMarker);
+    draggableMarker.addListener('dragend', (event) => {
+        const position = draggableMarker.position;
+        console.log(`Pin dropped at: ${position.lat}, ${position.lng}`);
+    });
 }
-//initMap();
-
-
-
 
 
 function getLocation() {
@@ -39,45 +37,49 @@ function getLocation() {
         const { latitude, longitude } = position.coords;
         console.log(position.coords);
         console.log(latitude, longitude);
-        showPosition(latitude, longitude);
-        //getDistanceFromLatLonInFt(position)
-        // Show a map centered at latitude / longitude.
-    }); 
+        initMap(latitude, longitude);
+    });
 }
 
+async function getDistanceFromLatLonInFt() {
+    navigator.geolocation.getCurrentPosition(position => {
+        var lat1 = position.coords.latitude;
+        var lon1 = position.coords.longitude;
+        //const { lat1, lon1 } = position.coords;
+        var lat2 = document.querySelector('gmp-advanced-marker').position.lat;
+        var lon2 = document.querySelector('gmp-advanced-marker').position.lng;
+        //console.log(lat1, lon1);
+        //console.log(lat2, lon2);
+        var R = 6371; // Radius of the earth in km
+        var dLat = deg2rad(lat2-lat1);  // deg2rad below
+        var dLon = deg2rad(lon2-lon1);
+        var a = 
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+        Math.sin(dLon/2) * Math.sin(dLon/2); 
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+        var d = R * c; // Distance in km
+        ft = d * 3280.84; // km to ft
+        //console.log(R, dLat, dLon, a, c, d, ft);
+        console.log(ft);
 
-
-function showPosition(lat, lon) {
-    let latlon = lat + "," + lon;
-    initMap(lat, lon);
-
-    //let img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false&markers=color:blue%7Clabel:S%7C"+latlon+"&key=AIzaSyAFvqraMHzsOODo2TnGG_OIJYC9OTrUepI";
-    //console.log(img_url);
-    //document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
-}
-
-
-function getDistanceFromLatLonInFt(pos1, pos2) {
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(pos2.coords.latitude-pos1.coords.latitude);  // deg2rad below
-  var dLon = deg2rad(pos2.coords.longitude-pos1.coords.longitude);
-  var a = 
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2); 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c; // Distance in km
-  var ft = d * 3280.84; // km to ft
-  return ft;
+        calculateScore(ft).then((score) => {
+            let p = document.createElement("p");
+            p.innerText = score;
+            document.body.append(p);
+        });
+        
+    });
+    
 }
 
 function deg2rad(deg) {
-  return deg * (Math.PI/180)
+    return deg * (Math.PI/180)
 }
 
 
 
-function calculateScore( dist ){
+async function calculateScore( dist ){
     var dif = 2;
     if (dist < 20) {
         return 1000;
@@ -88,4 +90,24 @@ function calculateScore( dist ){
     else {
         return 0;
     }
+}
+
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+async function showScore() {
+    /*getDistanceFromLatLonInFt().then((ft) => {
+        console.log("this happens instantly");
+        console.log(ft);
+        var score = calculateScore(distInFt);
+        let p = document.createElement("p");
+        p.innerText = score;
+        document.body.append(p);
+    });*/
+
+    getDistanceFromLatLonInFt();
+    //getDistanceFromLatLonInFt().then(x => { console.log(x); } );
+    //(async () => {console.log(await getDistanceFromLatLonInFt())})()
+
+    await delay(7000);
+    
 }
